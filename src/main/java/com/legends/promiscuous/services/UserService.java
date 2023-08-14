50c4b72962a40a1ec56b7de400b6ffde9472797e
OdogwuLegends <odigbocharlesjnrlegends@gmail.com>
2023-08-14 17:32:39 +0100

    added a new file

--- src/main/java/com/legends/promiscuous/services/UserService.java
+++ src/main/java/com/legends/promiscuous/services/UserService.java
@@ -4,6 +4,9 @@ import com.legends.promiscuous.dtos.requests.RegisterUserRequest;
 import com.legends.promiscuous.dtos.response.ApiResponse;
 import com.legends.promiscuous.dtos.response.GetUserResponse;
 import com.legends.promiscuous.dtos.response.RegisterUserResponse;
+import com.legends.promiscuous.models.User;
+
+import java.util.List;
 
 public interface UserService {
     RegisterUserResponse register(RegisterUserRequest service);
@@ -11,4 +14,6 @@ public interface UserService {
     ApiResponse<?> activateUserAccount(String token);
 
     GetUserResponse getUserById(long id);
+
+    List<User> getAllUsers(int page, int pageSize);
 }
--- src/test/java/com/legends/promiscuous/services/UserServiceTest.java
+++ src/test/java/com/legends/promiscuous/services/UserServiceTest.java
@@ -4,11 +4,14 @@ import com.legends.promiscuous.dtos.requests.RegisterUserRequest;
 import com.legends.promiscuous.dtos.response.ApiResponse;
 import com.legends.promiscuous.dtos.response.GetUserResponse;
 import com.legends.promiscuous.dtos.response.RegisterUserResponse;
+import com.legends.promiscuous.models.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -52,5 +55,9 @@ public class UserServiceTest {
         GetUserResponse response = userService.getUserById(1L);
         assertThat(response).isNotNull();
         assertThat(response.getEmail()).isEqualTo(registerUserRequest.getEmail());
+    }
+    @Test
+    public void getAllUsers(){
+        List<User> users = userService.getAllUsers(1,5);
     }
 }
