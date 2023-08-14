ba43d07394e258a296d89f8064c8f7ce2a158d0b
OdogwuLegends <odigbocharlesjnrlegends@gmail.com>
2023-08-14 12:15:28 +0100

    added a new file

--- src/main/java/com/legends/promiscuous/exceptions/ExceptionMessage.java
+++ src/main/java/com/legends/promiscuous/exceptions/ExceptionMessage.java
@@ -2,8 +2,8 @@ package com.legends.promiscuous.exceptions;
 
 public enum ExceptionMessage {
 
-    USER_NOT_FOUND_EXCEPTION;
-    USER_NOT_FOUND_EXCEPTION("")
+    USER_NOT_FOUND_EXCEPTION,
+    USER_WITH_EMAIL_NOT_FOUND_EXCEPTION("User with email %s not found");
     ExceptionMessage(){
 
     }
