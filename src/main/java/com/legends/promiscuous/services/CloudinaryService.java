8209f49128850071d82e9972b619a7b8723bab2d
OdogwuLegends <odigbocharlesjnrlegends@gmail.com>
2023-08-22 18:02:38 +0100

    added new file

--- src/main/java/com/legends/promiscuous/services/CloudinaryService.java
+++ src/main/java/com/legends/promiscuous/services/CloudinaryService.java
@@ -1,13 +1,31 @@
 package com.legends.promiscuous.services;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.Uploader;
+import com.cloudinary.utils.ObjectUtils;
 import com.legends.promiscuous.dtos.response.ApiResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Service
 public class CloudinaryService implements CloudService{
     @Override
     public ApiResponse<String> upload(MultipartFile file) {
+        Cloudinary cloudinary = new Cloudinary();
+        Uploader uploader = cloudinary.uploader();
+        try{
+            uploader.upload(file.getBytes(), ObjectUtils.asMap(
+                    "public_id","",
+                    "api_key", "",
+                    "api_secret", "",
+                    "cloud_name", "",
+                    "secure", true
+            ))
+        }catch (IOException exception){
+            throw new RuntimeException("File upload failed");
+        }
         return null;
     }
 }
