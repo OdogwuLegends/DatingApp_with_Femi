268fa8687a1a6cdf42e56316b2ed64b141200feb
OdogwuLegends <odigbocharlesjnrlegends@gmail.com>
2023-08-25 09:25:34 +0100

    added a new file

--- src/main/java/com/legends/promiscuous/security/provider/PromiscuousAuthenticationProvider.java
+++ src/main/java/com/legends/promiscuous/security/provider/PromiscuousAuthenticationProvider.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -15,8 +16,24 @@ public class PromiscuousAuthenticationProvider implements AuthenticationProvider
     private final UserDetailsService userDetailsService;
     private final PasswordEncoder passwordEncoder;
 
+
+
+
+
+    //3. if the passwords match, request is authenticated
+
+    //4. if the passwords don't match, request is not authenticated'
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        //1. take the username from the request(contained in the authentication object) and use
+        //the userDetailsService to look for a user from the DB with that username
+        String email = authentication.getPrincipal().toString();
+        UserDetails user = userDetailsService.loadUserByUsername(email);
+
+        //2. if user from 1. is found, use the PasswordEncoder to compare the password from the
+        //request with the password from the DB
+
+        String password = authentication.getCredentials().toString();
         return null;
     }
 
